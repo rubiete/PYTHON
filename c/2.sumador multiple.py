91afae550eedef0c261d163101f38898f161ee3b
rubiete <33655421+rubiete@users.noreply.github.com>
2017-11-16 11:56:41 +0100

    Add files via upload

---
+++ c/2.sumador multiple.py
@@ -0,0 +1,8 @@
+def er():
+    suma=0
+    for cont in  range(1,10,2):
+        suma=suma+cont
+
+    print suma
+            
+er()        
---
+++ c/3.tabla de multiplicar.py
@@ -0,0 +1,6 @@
+def tablademultiplicar():
+    numero= input("INTRODUCE EL NUMERO DE LA TABLA QUE DESEAS: ")
+    for cont in  range(1,462174675,1):
+        print numero, "x", cont,"=",numero*cont
+        
+tablademultiplicar()
