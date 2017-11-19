364bd7236d2136873ddd4ceeda12a3079bd6ba8b
rubiete <33655421+rubiete@users.noreply.github.com>
2017-11-19 20:12:20 +0100

    Delete 13.contapares.py

--- c/13.contapares.py
+++
@@ -1,9 +0,0 @@
-def contapares():
-    numero= input("Introduce el número: ")
-    pares=0
-    while numero>0:
-        if (numero%2)==0:
-            pares=pares+1
-        numero=numero/10
-    print "Su número tiene", pares,"cifras pares."    
-contapares()
