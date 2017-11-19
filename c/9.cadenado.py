33c1aff935b658e902396e9c172e4bf71104915e
rubiete <33655421+rubiete@users.noreply.github.com>
2017-11-19 20:13:40 +0100

    Delete 9.cadenado.py

--- c/9.cadenado.py
+++
@@ -1,7 +0,0 @@
-def cadenador():
-    nombre=raw_input("Dime tu nombre: ")
-    apellido1=raw_input("Dime tu primer apellido: ")
-    apellido2=raw_input("Dime tu segundo apellido: ")
-    print "Tu nombre completo es" ,nombre,apellido1,apellido2,
-
-cadenador()
