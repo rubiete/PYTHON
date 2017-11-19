64c884099d1b9a82229fc011d6e0a920d3aa185c
rubiete <33655421+rubiete@users.noreply.github.com>
2017-11-19 20:13:01 +0100

    Delete 4.sucesion.py

--- c/4.sucesion.py
+++
@@ -1,8 +0,0 @@
-def sucesionador():
-    numero= input("INTRODUCE EL NUMERO HASTA EL QUE DESEAS SUMAR: ")
-    suma=0
-    for cont in  range(1,numero+1,1):
-        suma=suma+cont
-    print "resultado=", suma
-        
-sucesionador()
