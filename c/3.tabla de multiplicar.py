f28198efb673e3eee5521a27253821054905deef
rubiete <33655421+rubiete@users.noreply.github.com>
2017-11-19 20:12:55 +0100

    Delete 3.tabla de multiplicar.py

--- c/3.tabla de multiplicar.py
+++
@@ -1,6 +0,0 @@
-def tablademultiplicar():
-    numero= input("INTRODUCE EL NUMERO DE LA TABLA QUE DESEAS: ")
-    for cont in  range(1,462174675,1):
-        print numero, "x", cont,"=",numero*cont
-        
-tablademultiplicar()
